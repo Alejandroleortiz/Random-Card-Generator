c450bc4e8b8ec61dcdb17bd0eb52a21e0ada1281
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-01 16:31:20 +0000

    Formacion-cartas-html

--- src/app.js
+++ src/app.js
@@ -2,48 +2,44 @@
 import "./style.css";
 
 window.onload = function() {
-  //write your code here
+  const suits = ["♦", "♥", "♠", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const mazoCartas = [];
+
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      let value = values[v];
+      let suit = suits[s];
+      mazoCartas.push({ value, suit });
+    }
+  }
+
+  let card = mazoCartas[Math.floor(Math.random(mazoCartas.length) * 52)];
+
+  console.log("La carta es " + JSON.stringify(card));
+
+  let cartaValor = document.getElementById("valor");
+  cartaValor.innerHTML = card.value;
+
+  let cartaSimbolo = document.getElementById("simbolo1");
+  cartaSimbolo.innerHTML = card.suit;
+
+  let cartaSimbolo2 = document.getElementById("simbolo2");
+  cartaSimbolo2.innerHTML = card.suit;
+
   console.log("Todo esta funcionando");
 };
-
-const suits = ["♦", "♥", "♠", "♣"];
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-const mazoCartas = [];
-
-//function cardBuilder() {
-for (let s = 0; s < suits.length; s++) {
-  for (let v = 0; v < values.length; v++) {
-    let value = values[v];
-    let suit = suits[s];
-    mazoCartas.push({ value, suit });
-  }
-}
-//return cards;
-//}
-
-console.log(
-  "La carta es " +
-    JSON.stringify(mazoCartas[Math.floor(Math.random(mazoCartas.length) * 52)])
-);
-
-/*let card = cardBuilder();
-
-let randomCards = () => {
-  return Math.floor(Math.random(card) * 52);
-};
-
-console.log(randomCards(cards));*/
