9d97bfcecd594f3287ff1b796ea8cad7ca9038d0
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-01 13:55:17 +0000

    finalizada la function

--- src/app.js
+++ src/app.js
@@ -5,3 +5,33 @@ window.onload = function() {
   //write your code here
   console.log("Todo esta funcionando");
 };
+
+function cardBuilder() {
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["♦", "♥", "♠", "♣"];
+  const cards = [];
+
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      let value = values[v];
+      let suit = suits[s];
+      cards.push({ value, suit });
+    }
+  }
+  return cards;
+}
+console.log(cardBuilder());
